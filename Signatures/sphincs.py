3c336db434eca3e98a97c1167a30fab45a9e522b
Anthony Mikhaiel <c3037092@newcastle.ac.uk>
2026-04-10 02:49:38 +0300

    sphincs

---
+++ Signatures/sphincs.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+if sys.platform == "win32":
+    dll_dir = r"C:\Users\mtony\PycharmProjects\csc3094_blockchain\liboqs\cmake-build-debug\bin"
+    if os.path.isdir(dll_dir):
+        os.environ["PATH"] = dll_dir + os.pathsep + os.environ.get("PATH", "")
+        os.add_dll_directory(dll_dir)
+
+import oqs
+
+
+class SPHINCSSignature:
+    NAME = "SLH_DSA_PURE_SHA2_128S"
+    ALGORITHM = "SPHINCS+"
+    STANDARD = "FIPS 205 (SLH-DSA)"
+    SECURITY_LEVEL = 1  # ~128-bit classical / ~64-bit quantum
+
+    def generate_keypair(self):
+        with oqs.Signature(self.NAME) as s:
+            public_key = s.generate_keypair()
+            secret_key = s.export_secret_key()
+        return secret_key, public_key
+
+    def sign(self, secret_key, message):
+        with oqs.Signature(self.NAME, secret_key) as s:
+            return s.sign(message)
+
+    def verify(self, public_key, message, signature):
+        try:
+            with oqs.Signature(self.NAME) as v:
+                return v.verify(message, signature, public_key)
+        except Exception:
+            return False
