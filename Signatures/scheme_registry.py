0ddda89bedc97c9a19075b6834a113b5af03a369
Anthony Mikhaiel <c3037092@newcastle.ac.uk>
2026-04-10 13:51:53 +0300

    scheme registry update

--- Signatures/scheme_registry.py
+++ Signatures/scheme_registry.py
@@ -1,7 +1,11 @@
-from Signatures.dilithium import DilithiumSignature
 from Signatures.ecdsa import ECDSASignature
+from Signatures.eddsa import EdDSASignature
+from Signatures.dilithium import DilithiumSignature
+from Signatures.falcon import FalconSignature
 
 scheme_registry = {
     ECDSASignature.NAME: ECDSASignature(),
+    EdDSASignature.NAME: EdDSASignature(),
     DilithiumSignature.NAME: DilithiumSignature(),
+    FalconSignature.NAME: FalconSignature(),
 }
