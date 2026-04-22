c8c534ed35c25b90f5a0ffb9d47e65e714150b0b
Anthony Mikhaiel <c3037092@newcastle.ac.uk>
2026-04-22 15:04:41 +0100

    removal of eddsa

--- Signatures/scheme_registry.py
+++ Signatures/scheme_registry.py
@@ -1,11 +1,9 @@
 from Signatures.ecdsa import ECDSASignature
-from Signatures.eddsa import EdDSASignature
 from Signatures.dilithium import DilithiumSignature
 from Signatures.falcon import FalconSignature
 
 scheme_registry = {
     ECDSASignature.NAME: ECDSASignature(),
-    EdDSASignature.NAME: EdDSASignature(),
     DilithiumSignature.NAME: DilithiumSignature(),
     FalconSignature.NAME: FalconSignature(),
 }
